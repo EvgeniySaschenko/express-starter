12cab8874ab82092634f968ae81efe7f9eb79091
Evgeniy Saschenko <evgeniy.saschenko@gmail.com>
2021-12-13 00:35:36 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "prettier",
+  ],
+
+
+  parser: "babel-eslint",
+
+  parserOptions: {},
+
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prefer-const": "off",
+    "no-unused-vars": "warn",
+    "unicorn/error-message": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    eqeqeq: "warn",
+    "no-var": "error",
+  },
+};
