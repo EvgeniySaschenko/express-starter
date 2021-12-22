51c0470061364a7ffac18b87451e1d46a386f646
Evgeniy Saschenko <evgeniy.saschenko@gmail.com>
2021-12-22 23:38:09 +0200

    user

---
+++ class/user.js
@@ -0,0 +1,9 @@
+let { M_Users } = require(ROOT_PATH + "/models/users.js");
+let passport = require("passport");
+let PassportLocal = require("passport-local");
+
+console.log(PassportLocal);
+
+class User {
+  getUserByEmail(userName) {}
+}
--- models/_create-tables.js
+++ models/_create-tables.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 let db = require("./_db");
 let users = require("./users");
 let models = [users];
-// node models/_createTables.js
+// node models/_create-tables.js
 // Создаём таблицы в БД
 for (let model of models) {
   db.getQueryInterface().createTable("users", model.table);
